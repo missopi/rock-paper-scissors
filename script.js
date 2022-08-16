9cfed1904d669c06a5d57da2891cb136cd2203e0
missopi <missopi@me.com>
2022-08-16 20:43:20 +0100

    Add playRound function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // function to get player's selection //
 
-let playerSelection = prompt('What do you choose? Rock, Paper, or Scissors?');
+let playerSelection = prompt('What do you choose? Rock, Paper, or Scissors?').toLocaleLowerCase();
 if (playerSelection === 'rock' || playerSelection === 'scissors' || playerSelection === 'paper') {
 console.log("You chose " + playerSelection);
 } else {
@@ -15,21 +15,22 @@ let computerSelection = choice[Math.floor(Math.random() * 3)];
 console.log("Computer chooses " + computerSelection);
 
 
-// function to decide winner //
- 
-if (playerSelection === 'rock' && computerSelection === 'paper') {
-    console.log('You Lost. Paper covers Rock.');  
-} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    console.log('You Lost. Scissors cut Paper.');
-} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    console.log('You Lost. Rock breaks Scissors.');
-} else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    console.log('You Won! Paper covers Rock.');
-} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    console.log('You Won! Scissors cut Paper.');
-} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    console.log('You Won! Rock breaks Scissors.');
-} else {
-    console.log('It is a draw');
-}; 
+// function to play round //
 
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+    console.log('You Lost. Paper covers Rock.');  
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lost. Scissors cut Paper.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lost. Rock breaks Scissors.';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Won! Paper covers Rock.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Won! Scissors cut Paper.';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Won! Rock breaks Scissors.';
+    } else {
+        return 'It is a draw';
+    }; 
+}
