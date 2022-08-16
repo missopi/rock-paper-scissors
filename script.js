25e3d4563691bcff9849215fa05d938cc5b0d91d
missopi <missopi@me.com>
2022-08-16 12:12:50 +0100

    Change wording in computer choice function

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 // function to get computer's choice //
 function getComputerChoice() {
-    let compChoice = Math.floor(Math.random() * 3);
-    console.log('Computer Choice: ' + compChoice);
+    let compSelection = Math.floor(Math.random() * 3);
+    console.log('Computer Selection: ' + compSelection);
     var rps;
     // switch integer value to either rock, paper or scissors //
-    switch (compChoice) {
+    switch (compSelection) {
       case 0:
         rps = 'Rock';
         break;
