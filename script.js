b376cfa73c4fcf95bc29355d7cf3725800b73e85
missopi <missopi@me.com>
2022-08-26 15:42:42 +0100

    Return game function

--- script.js
+++ script.js
@@ -66,22 +66,22 @@ let finish = ["You Lost", "You Won!", "It's a draw.","Invalid Choice"];
 function playRound(playerSelection, computerSelection) {
     let result = "";
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        //computerScore += 1;
+        computerScore += 1;
         result = finish[0];
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        //computerScore += 1;
+        computerScore += 1;
         result = finish[0];
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        //computerScore += 1;
+        computerScore += 1;
         result = finish[0];
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        //playerScore += 1;
+        playerScore += 1;
         result = finish[1];
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        //playerScore += 1;
+        playerScore += 1;
         result = finish[1];
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        //playerScore += 1;
+        playerScore += 1;
         result = finish[1];
     } else if (playerSelection === computerSelection) {
         result = finish[2];
@@ -92,37 +92,37 @@ function playRound(playerSelection, computerSelection) {
 };
 
 // function to play a 5 round game 
-//function game() {
-//    for (let i = 0; i < 5; i++) {
-//        let playerScore = 0;
-//        let computerScore = 0;
-//        let playerSelection = getPlayerSelection();
-//        console.log(playerSelection);
-//        let computerSelection = getComputerChoice();
-//        console.log(computerSelection);
-//        let result = playRound(playerSelection, computerSelection);
-//        console.log(result);
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerScore = 0;
+        let computerScore = 0;
+        let playerSelection = getPlayerSelection();
+        console.log(playerSelection);
+        let computerSelection = getPlayerSelection();
+        console.log(computerSelection);
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
         
-//        if (result === "You Lost. Paper covers rock. Your score = " + playerScore + ". The computer's score = " + computerScore || 
-//        result === "You Lost. Scissors cut paper. Your score = " + playerScore + ". The computer's score = " + computerScore || 
-//        result === "You Lost. Rock breaks scissors. Your score = " + playerScore + ". The computer's score = " + computerScore) {
-//            computerScore + 1;
-//        }
-//        else if (result === "You Won! Paper covers rock. Your score = " + playerScore + ". The computer's score = " + computerScore || 
-//        result === "You Won! Scissors cut paper. Your score = " + playerScore + ". The computer's score = " + computerScore || 
-//        result === "You Won! Rock breaks scissors. Your score = " + playerScore + ". The computer's score = " + computerScore) {
-//            playerScore + 1;
-//        }
-//    }
-//    if (playerScore < computerScore){
-//        return "You lose. Your score = " + playerScore + ". The computer's score = " + computerScore;
-//    }
-//    else if (playerScore > computerScore) {
-//        return "You win! Your score = " + playerScore + ". The computer's score = " + computerScore;
-//    }
-//    else {
-//        return "It is a draw. Your score = " + playerScore + ". The computer's score = " + computerScore;
-//    }
-//}
+        if (result === "You Lost. Paper covers rock. Your score = " + playerScore + ". The computer's score = " + computerScore || 
+        result === "You Lost. Scissors cut paper. Your score = " + playerScore + ". The computer's score = " + computerScore || 
+        result === "You Lost. Rock breaks scissors. Your score = " + playerScore + ". The computer's score = " + computerScore) {
+            computerScore + 1;
+        }
+        else if (result === "You Won! Paper covers rock. Your score = " + playerScore + ". The computer's score = " + computerScore || 
+        result === "You Won! Scissors cut paper. Your score = " + playerScore + ". The computer's score = " + computerScore || 
+        result === "You Won! Rock breaks scissors. Your score = " + playerScore + ". The computer's score = " + computerScore) {
+            playerScore + 1;
+        }
+    }
+    if (playerScore < computerScore){
+        return "You lose. Your score = " + playerScore + ". The computer's score = " + computerScore;
+    }
+    else if (playerScore > computerScore) {
+        return "You win! Your score = " + playerScore + ". The computer's score = " + computerScore;
+    }
+    else {
+        return "It is a draw. Your score = " + playerScore + ". The computer's score = " + computerScore;
+    }
+}
 
 //console.log(game());
